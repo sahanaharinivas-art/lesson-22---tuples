39d9eef606086ba5c31c5100fa4978f9b5125395
harinivassahana-max <harinivassahana@gmail.com>
2026-01-06 10:53:36 +0530

    activity 2

---
+++ activity 2.py
@@ -0,0 +1,16 @@
+def palind(r):
+    e = len(r) -1
+    s = 0
+    while(s<e):
+        if(r[s]!=r[e]):
+            return False
+        s+=1
+        e-=1
+    return True
+
+r = (1,2,3,3,2,1)
+
+if (palind(r)):
+    print("the tuple is Flip-Flop")
+else:
+    print("the tuple is not Flip-Flop")
